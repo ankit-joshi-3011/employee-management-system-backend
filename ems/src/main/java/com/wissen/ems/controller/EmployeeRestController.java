182bf453af6f58242a208e8d82c15231e498e68c
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-18 17:59:57 +0530

    Added the EmployeeRestController class that will expose our REST API endpoints that deal with employees. For now, it has a single endpoint to help create a new employee.

---
+++ ems/src/main/java/com/wissen/ems/controller/EmployeeRestController.java
@@ -0,0 +1,28 @@
+package com.wissen.ems.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wissen.ems.dto.EmployeeDetailsDTO;
+import com.wissen.ems.entity.Employee;
+import com.wissen.ems.service.EmployeeService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/employees")
+@AllArgsConstructor
+public class EmployeeRestController {
+	private EmployeeService employeeService;
+
+	@PostMapping
+	public ResponseEntity<Employee> createEmployee(@RequestBody EmployeeDetailsDTO employeeDetailsDTO) {
+		Employee createdEmployee = employeeService.save(employeeDetailsDTO);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdEmployee);
+	}
+}
