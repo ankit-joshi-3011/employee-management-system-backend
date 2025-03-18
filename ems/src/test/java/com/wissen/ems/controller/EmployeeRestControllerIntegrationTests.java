a98dc0e5ed2125bc891563bda8ca6d6fa4ebd62e
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-18 20:36:52 +0530

    Tested the scenario where the createEmployee() method of the EmployeeRestController class is passed in a DTO object wherein the regular employee is incorrectly reporting to the CEO (or another regular employee)

--- ems/src/test/java/com/wissen/ems/controller/EmployeeRestControllerIntegrationTests.java
+++ ems/src/test/java/com/wissen/ems/controller/EmployeeRestControllerIntegrationTests.java
@@ -148,4 +148,24 @@ public class EmployeeRestControllerIntegrationTests {
 
 		AssertionsForClassTypes.assertThat(responseBody).isEqualTo(Constants.INVALID_MANAGER_ID_EXCEPTION_MESSAGE);
 	}
+
+	@Test
+	public void testCreateRegularEmployeeReportingDirectlyToTheCeo() throws Exception {
+		RegularEmployeeDetailsDTO regularEmployeeDetailsDTO = new RegularEmployeeDetailsDTO();
+
+		regularEmployeeDetailsDTO.setName("CDE");
+		regularEmployeeDetailsDTO.setJobTitle("Principal Software Engineer");
+		regularEmployeeDetailsDTO.setDepartmentId(1);
+		regularEmployeeDetailsDTO.setManagerId(1);
+
+		MvcResult result = mockMvc.perform(post(Constants.EMPLOYEE_REST_API_BASE_URI_PATH)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(new ObjectMapper().writeValueAsString(regularEmployeeDetailsDTO)))
+			.andExpect(status().isUnprocessableEntity())
+			.andReturn();
+
+		String responseBody = result.getResponse().getContentAsString();
+
+		AssertionsForClassTypes.assertThat(responseBody).isEqualTo(Constants.REGULAR_EMPLOYEE_REPORTING_TO_REGULAR_EMPLOYEE_OR_CEO_EXCEPTION_MESSAGE);
+	}
 }
