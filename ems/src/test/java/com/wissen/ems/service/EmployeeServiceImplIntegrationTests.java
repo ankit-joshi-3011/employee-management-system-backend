38a32386815dd383a20ada302b437105d166aef5
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-18 16:44:03 +0530

    Added the integration test class which tests out the methods in the EmployeeServiceImpl class

---
+++ ems/src/test/java/com/wissen/ems/service/EmployeeServiceImplIntegrationTests.java
@@ -0,0 +1,55 @@
+package com.wissen.ems.service;
+
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wissen.ems.dto.RegularEmployeeDetailsDTO;
+import com.wissen.ems.entity.Employee;
+import com.wissen.ems.entity.EmployeeType;
+import com.wissen.ems.entity.EmploymentStatus;
+
+// Load the entire application context, so the real database & services are used
+@SpringBootTest
+// Ensure that database changes are rolled back after each test execution
+@Transactional
+public class EmployeeServiceImplIntegrationTests {
+	private EmployeeServiceImpl employeeService;
+
+	@Autowired
+	public EmployeeServiceImplIntegrationTests(EmployeeServiceImpl employeeService) {
+		this.employeeService = employeeService;
+	}
+
+	@Test
+	public void testCreateRegularEmployeeSuccessfully() {
+		RegularEmployeeDetailsDTO regularEmployeeDetailsDTO = new RegularEmployeeDetailsDTO();
+
+		String newEmployeeName = "WXY";
+		regularEmployeeDetailsDTO.setName(newEmployeeName);
+
+		String newEmployeeJobTitle = "Financial Management Associate";
+		regularEmployeeDetailsDTO.setJobTitle(newEmployeeJobTitle);
+
+		int financeDepartmentId = 3;
+		regularEmployeeDetailsDTO.setDepartmentId(financeDepartmentId);
+
+		regularEmployeeDetailsDTO.setEmployeeType("REGULAR");
+
+		int financeManagerEmployeeId = 11;
+		regularEmployeeDetailsDTO.setManagerId(financeManagerEmployeeId);
+
+		Employee createdEmployee = employeeService.save(regularEmployeeDetailsDTO);
+
+		AssertionsForClassTypes.assertThat(createdEmployee.getId()).isGreaterThan(0);
+		AssertionsForClassTypes.assertThat(createdEmployee.getName()).isEqualTo(newEmployeeName);
+		AssertionsForClassTypes.assertThat(createdEmployee.getJobTitle()).isEqualTo(newEmployeeJobTitle);
+		AssertionsForClassTypes.assertThat(createdEmployee.getDepartment().getId()).isEqualTo(financeDepartmentId);
+		AssertionsForClassTypes.assertThat(createdEmployee.getType()).isEqualTo(EmployeeType.REGULAR);
+		AssertionsForClassTypes.assertThat(createdEmployee.getStatus()).isEqualTo(EmploymentStatus.ACTIVE);
+		AssertionsForClassTypes.assertThat(createdEmployee.getManager().getId()).isEqualTo(financeManagerEmployeeId);
+		AssertionsForClassTypes.assertThat(createdEmployee.getDirectReports()).isNull();
+	}
+}
