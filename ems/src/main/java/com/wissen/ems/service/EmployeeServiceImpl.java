3b369646bea434a4e23852302e62672964df3001
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-18 15:19:03 +0530

    Added the EmployeeServiceImpl class which is an implementation of the EmployeeService interface

---
+++ ems/src/main/java/com/wissen/ems/service/EmployeeServiceImpl.java
@@ -0,0 +1,28 @@
+package com.wissen.ems.service;
+
+import org.springframework.stereotype.Service;
+
+import com.wissen.ems.dto.EmployeeDetailsDTO;
+import com.wissen.ems.employeecreator.EmployeeCreator;
+import com.wissen.ems.entity.Employee;
+import com.wissen.ems.repository.EmployeeRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+	private EmployeeCreator employeeCreator;
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public Employee save(EmployeeDetailsDTO employeeDetailsDTO) {
+		if (!employeeCreator.supportsCreation(employeeDetailsDTO)) {
+			throw new IllegalArgumentException("Unsupported employee type");
+		}
+
+		Employee newEmployee = employeeCreator.create(employeeDetailsDTO);
+
+		return employeeRepository.save(newEmployee);
+	}
+}
