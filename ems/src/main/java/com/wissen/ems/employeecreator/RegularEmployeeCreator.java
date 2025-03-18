9367ce0154cd57c6c0f2e3b287840698aed74c0f
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-18 15:08:35 +0530

    Created an implementation of the EmployeeCreator interface called RegularEmployeeCreator which will help in creating a regular employee

---
+++ ems/src/main/java/com/wissen/ems/employeecreator/RegularEmployeeCreator.java
@@ -0,0 +1,75 @@
+package com.wissen.ems.employeecreator;
+
+import org.springframework.stereotype.Component;
+
+import com.wissen.ems.dto.EmployeeDetailsDTO;
+import com.wissen.ems.dto.RegularEmployeeDetailsDTO;
+import com.wissen.ems.entity.Department;
+import com.wissen.ems.entity.Employee;
+import com.wissen.ems.entity.EmployeeType;
+import com.wissen.ems.entity.EmploymentStatus;
+import com.wissen.ems.exception.BusinessRuleViolationException;
+import com.wissen.ems.repository.DepartmentRepository;
+import com.wissen.ems.repository.EmployeeRepository;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class RegularEmployeeCreator implements EmployeeCreator {
+	private DepartmentRepository departmentRepository;
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public boolean supportsCreation(EmployeeDetailsDTO employeeDetailsDto) {
+		return employeeDetailsDto instanceof RegularEmployeeDetailsDTO;
+	}
+
+	@Override
+	public Employee create(EmployeeDetailsDTO employeeDetailsDto) {
+		if (!supportsCreation(employeeDetailsDto)) {
+			throw new IllegalArgumentException("Incorrect argument passed to creator. Expected: RegularEmployeeDetailsDTO, Found: " + employeeDetailsDto.getClass().getSimpleName());
+		}
+
+		RegularEmployeeDetailsDTO regularEmployeeDetailsDTO = (RegularEmployeeDetailsDTO) employeeDetailsDto;
+
+		String name = regularEmployeeDetailsDTO.getName();
+
+		if (name == null || name.isEmpty() || name.isBlank()) {
+			throw new IllegalArgumentException("Employee's name cannot be null or empty");
+		}
+
+		String jobTitle = regularEmployeeDetailsDTO.getJobTitle();
+
+		if (jobTitle == null || jobTitle.isEmpty() || jobTitle.isBlank()) {
+			throw new IllegalArgumentException("Employee's job title cannot be null or empty");
+		}
+
+		Department department = departmentRepository.findById(regularEmployeeDetailsDTO.getDepartmentId())
+			.orElseThrow(() -> new BusinessRuleViolationException("A regular employee should belong to an existing department"));
+
+		Employee manager = employeeRepository.findById(regularEmployeeDetailsDTO.getManagerId())
+			.orElseThrow(() -> new BusinessRuleViolationException("A regular employee should report to an existing manager"));
+
+		EmployeeType managerType = manager.getType();
+
+		if (managerType == EmployeeType.REGULAR || managerType == EmployeeType.CEO) {
+			throw new BusinessRuleViolationException("A regular employee cannot report to another regular employee or directly to the CEO");
+		}
+
+		if (manager.getDepartment().getId() != department.getId()) {
+			throw new BusinessRuleViolationException("A regular employee cannot report to a manager in another department");
+		}
+
+		Employee newRegularEmployee = new Employee();
+
+		newRegularEmployee.setName(name);
+		newRegularEmployee.setJobTitle(jobTitle);
+		newRegularEmployee.setDepartment(department);
+		newRegularEmployee.setType(EmployeeType.REGULAR);
+		newRegularEmployee.setStatus(EmploymentStatus.ACTIVE);
+		newRegularEmployee.setManager(manager);
+
+		return employeeRepository.save(newRegularEmployee);
+	}
+}
