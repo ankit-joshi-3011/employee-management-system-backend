adc6574a15a818e6a8ab34458ccd5f99b7c31631
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-19 03:48:16 +0530

    Added the CorsConfiguration class mainly to customize the default Spring MVC configuration. We need to modify the CORS settings to allow the front-end to be able to access the back-end. Without this modification, the browser will block the front-end from making API requests to the back-end due to the Same-Origin Policy.

---
+++ ems/src/main/java/com/wissen/ems/configuration/CorsConfiguration.java
@@ -0,0 +1,34 @@
+package com.wissen.ems.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.wissen.ems.common.Constants;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@AllArgsConstructor
+public class CorsConfiguration {
+	private EmployeeManagementSystemConfigurationProperties configurationProperties;
+
+	// WebMvcConfigurer allows us to customize the default Spring MVC configuration.
+	// We need to modify the CORS settings to allow the front-end to be able to
+	// access the back-end. Without this modification, the browser will block the
+	// front-end from making API requests to the back-end due to the Same-Origin
+	// Policy.
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping(Constants.EMPLOYEE_REST_API_BASE_URI_PATH)
+					.allowedOrigins(configurationProperties.getFrontendUrl())
+					.allowedMethods("POST")
+					.allowedHeaders("*");
+			}
+		};
+	}
+}
