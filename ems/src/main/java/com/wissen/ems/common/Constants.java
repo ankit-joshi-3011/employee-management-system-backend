20009d98e4ee8a4dfa5d0928ef66d2e3882f074f
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-18 16:58:10 +0530

    Added a class called Constants that will store all the constant values that we use across the application. For now, it contains the message of the exception that is thrown when the EmployeeCreator does not support creating employees of a certain type.

---
+++ ems/src/main/java/com/wissen/ems/common/Constants.java
@@ -0,0 +1,8 @@
+package com.wissen.ems.common;
+
+public final class Constants {
+	private Constants() {
+	}
+
+	public static final String UNSUPPORTED_EMPLOYEE_TYPE_EXCEPTION_MESSAGE = "Unsupported employee type";
+}
