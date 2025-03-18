67c021f1c81ba887bc68b324aad36e3899ddcb9c
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-18 16:58:57 +0530

    Used the UNSUPPORTED_EMPLOYEE_TYPE_EXCEPTION_MESSAGE constant in the EmployeeServiceImpl class

--- ems/src/main/java/com/wissen/ems/service/EmployeeServiceImpl.java
+++ ems/src/main/java/com/wissen/ems/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.wissen.ems.service;
 
 import org.springframework.stereotype.Service;
 
+import com.wissen.ems.common.Constants;
 import com.wissen.ems.dto.EmployeeDetailsDTO;
 import com.wissen.ems.employeecreator.EmployeeCreator;
 import com.wissen.ems.entity.Employee;
@@ -18,7 +19,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Override
 	public Employee save(EmployeeDetailsDTO employeeDetailsDTO) {
 		if (!employeeCreator.supportsCreation(employeeDetailsDTO)) {
-			throw new IllegalArgumentException("Unsupported employee type");
+			throw new IllegalArgumentException(Constants.UNSUPPORTED_EMPLOYEE_TYPE_EXCEPTION_MESSAGE);
 		}
 
 		Employee newEmployee = employeeCreator.create(employeeDetailsDTO);
