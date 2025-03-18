7805cc097114d743c51db717ea53a95ca1d51693
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-18 12:41:45 +0530

    Updated the data.sql file to reflect the fact that the employees table now has a field to store the employee type. Also, added 3 more rows to the database of employees who are regular managers. The regular employees would now be reporting to the regular managers instead of directly reporting to their department heads.

--- ems/src/main/resources/data.sql
+++ ems/src/main/resources/data.sql
@@ -5,13 +5,13 @@
 -- in-memory H2 database is lost.
 
 -- Insert CEO
-INSERT INTO employees (name, job_title, department_id, status, manager_id) VALUES ('ABC', 'CEO', NULL, 'ACTIVE', NULL)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('ABC', 'CEO', NULL, 'CEO', 'ACTIVE', NULL)
 
 -- Create First Department
 INSERT INTO departments (name, head_id) VALUES ('Engineering', 1)
 
 -- Create the Head of Engineering
-INSERT INTO employees (name, job_title, department_id, status, manager_id) VALUES ('DEF', 'Head of Engineering', 1, 'ACTIVE', 1)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('DEF', 'Head of Engineering', 1, 'DEPARTMENT_HEAD', 'ACTIVE', 1)
 
 -- Make the above employee the Head of Engineering
 UPDATE departments set head_id = 2 where id = 1
@@ -20,7 +20,7 @@ UPDATE departments set head_id = 2 where id = 1
 INSERT INTO departments (name, head_id) VALUES ('HR', 1)
 
 -- Create the Head of HR
-INSERT INTO employees (name, job_title, department_id, status, manager_id) VALUES ('GHI', 'Head of HR', 2, 'ACTIVE', 1)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('GHI', 'Head of HR', 2, 'DEPARTMENT_HEAD', 'ACTIVE', 1)
 
 -- Make the above employee the Head of HR
 UPDATE departments set head_id = 3 where id = 2
@@ -29,17 +29,20 @@ UPDATE departments set head_id = 3 where id = 2
 INSERT INTO departments (name, head_id) VALUES ('Finance', 1)
 
 -- Create the Head of Finance
-INSERT INTO employees (name, job_title, department_id, status, manager_id) VALUES ('JKL', 'Head of Finance', 3, 'ACTIVE', 1)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('JKL', 'Head of Finance', 3, 'DEPARTMENT_HEAD', 'ACTIVE', 1)
 
 -- Make the above employee the Head of Finance
 UPDATE departments set head_id = 4 where id = 3
 
 -- Insert Regular Employees
-INSERT INTO employees (name, job_title, department_id, status, manager_id) VALUES ('MNO', 'Principal Software Engineer', 1, 'ACTIVE', 2)
-INSERT INTO employees (name, job_title, department_id, status, manager_id) VALUES ('PQR', 'Software Engineer', 1, 'ACTIVE', 2)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('MNO', 'Software Engineering Manager', 1, 'REGULAR_MANAGER', 'ACTIVE', 2)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('PQR', 'Principal Software Engineer', 1, 'REGULAR', 'ACTIVE', 5)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('STU', 'Software Engineer', 1, 'REGULAR', 'ACTIVE', 5)
 
-INSERT INTO employees (name, job_title, department_id, status, manager_id) VALUES ('STU', 'HR Specialist', 2, 'ACTIVE', 3)
-INSERT INTO employees (name, job_title, department_id, status, manager_id) VALUES ('STU', 'HR Coordinator', 2, 'ACTIVE', 3)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('VWX', 'HR Manager', 2, 'REGULAR_MANAGER', 'ACTIVE', 3)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('YZA', 'HR Specialist', 2, 'REGULAR', 'ACTIVE', 8)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('BCD', 'HR Coordinator', 2, 'REGULAR', 'ACTIVE', 8)
 
-INSERT INTO employees (name, job_title, department_id, status, manager_id) VALUES ('VWX', 'Finance Analyst', 3, 'ACTIVE', 4)
-INSERT INTO employees (name, job_title, department_id, status, manager_id) VALUES ('YZA', 'Accountant', 3, 'ACTIVE', 4)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('EFG', 'Finance Manager', 3, 'REGULAR_MANAGER', 'ACTIVE', 4)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('HIJ', 'Finance Analyst', 3, 'REGULAR', 'ACTIVE', 11)
+INSERT INTO employees (name, job_title, department_id, type, status, manager_id) VALUES ('KLM', 'Accountant', 3, 'REGULAR', 'ACTIVE', 11)
