7a9086735666c05cf1e365639ec5c0adcf205dff
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-19 11:37:26 +0530

    Made the id and the name fields non-final. Also added the @Setter annotation so that we can use the setter methods in the test classes.

--- ems/src/main/java/com/wissen/ems/dto/DepartmentDetailsDTO.java
+++ ems/src/main/java/com/wissen/ems/dto/DepartmentDetailsDTO.java
@@ -3,11 +3,13 @@ package com.wissen.ems.dto;
 import com.wissen.ems.entity.Department;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class DepartmentDetailsDTO {
-	private final int id;
-	private final String name;
+	private int id;
+	private String name;
 
 	public DepartmentDetailsDTO(Department department) {
 		id = department.getId();
