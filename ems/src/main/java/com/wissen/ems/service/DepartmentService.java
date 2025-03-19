54289f0159b8d8d687b1199808c8e73d249c33b7
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-19 10:44:18 +0530

    Added the DepartmentService interface that will act as a contract which should be adhered to by a service that interacts with the departments entity

---
+++ ems/src/main/java/com/wissen/ems/service/DepartmentService.java
@@ -0,0 +1,9 @@
+package com.wissen.ems.service;
+
+import java.util.List;
+
+import com.wissen.ems.dto.DepartmentDetailsDTO;
+
+public interface DepartmentService {
+	List<DepartmentDetailsDTO> getAllDepartments();
+}
