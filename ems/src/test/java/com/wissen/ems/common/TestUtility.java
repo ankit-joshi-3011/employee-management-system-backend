dd9bc79562452a091f5e4eae48e0bb043c17f9ba
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-19 13:39:01 +0530

    Added a utility method inside the TestUtility class to validate that the ManagerDetailsRetrievalDTO objects passed into it are correct

--- ems/src/test/java/com/wissen/ems/common/TestUtility.java
+++ ems/src/test/java/com/wissen/ems/common/TestUtility.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.assertj.core.api.AssertionsForClassTypes;
 
 import com.wissen.ems.dto.DepartmentDetailsDTO;
+import com.wissen.ems.dto.ManagerDetailsRetrievalDTO;
 
 public final class TestUtility {
 	private TestUtility() {
@@ -21,6 +22,20 @@ public final class TestUtility {
 
 			AssertionsForClassTypes.assertThat(departmentDetailsDto.getId()).isEqualTo(departmentIds[i]);
 			AssertionsForClassTypes.assertThat(departmentDetailsDto.getName()).isEqualTo(departmentNames[i]);
+		}
+	}
+
+	public static void assertManagerDetailsRetrievalDtosAreCorrect(List<ManagerDetailsRetrievalDTO> responseActiveEngineeringManagersDto) {
+		int[] activeEngineeringManagerIds = new int[] { 2, 5 };
+		String[] activeEngineeringManagerNames = new String[] { "DEF", "MNO" };
+
+		AssertionsForClassTypes.assertThat(responseActiveEngineeringManagersDto.size()).isEqualTo(activeEngineeringManagerIds.length);
+
+		for (int i = 0; i < responseActiveEngineeringManagersDto.size(); i++) {
+			ManagerDetailsRetrievalDTO activeEngineeringManagerDto = responseActiveEngineeringManagersDto.get(i);
+
+			AssertionsForClassTypes.assertThat(activeEngineeringManagerDto.getId()).isEqualTo(activeEngineeringManagerIds[i]);
+			AssertionsForClassTypes.assertThat(activeEngineeringManagerDto.getName()).isEqualTo(activeEngineeringManagerNames[i]);
 		}
 	}
 }
