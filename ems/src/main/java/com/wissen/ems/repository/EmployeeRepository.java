08a6054a97e95932cffaecc143453028e395a00c
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-19 12:50:10 +0530

    Added a method to the EmployeeRepository that will help us to find all active managers (either regular managers or department heads) within a specific department

--- ems/src/main/java/com/wissen/ems/repository/EmployeeRepository.java
+++ ems/src/main/java/com/wissen/ems/repository/EmployeeRepository.java
@@ -1,10 +1,16 @@
 package com.wissen.ems.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.wissen.ems.entity.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	@Query("SELECT e FROM Employee e WHERE e.department.id = :departmentId AND (e.type = 'REGULAR_MANAGER' OR e.type = 'DEPARTMENT_HEAD') AND e.status = 'ACTIVE'")
+	List<Employee> findActiveManagersByDepartment(@Param("departmentId") int departmentId);
 }
