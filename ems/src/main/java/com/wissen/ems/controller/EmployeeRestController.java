091328486891f3e6ad479e4b56ac07f5507ae7b7
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-19 13:24:23 +0530

    Added the endpoint to retrieve active managers by department to the EmployeeRestController

--- ems/src/main/java/com/wissen/ems/controller/EmployeeRestController.java
+++ ems/src/main/java/com/wissen/ems/controller/EmployeeRestController.java
@@ -1,6 +1,7 @@
 package com.wissen.ems.controller;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +12,13 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wissen.ems.common.Constants;
 import com.wissen.ems.dto.EmployeeDetailsDTO;
+import com.wissen.ems.dto.ManagerDetailsRetrievalDTO;
 import com.wissen.ems.entity.Employee;
 import com.wissen.ems.service.EmployeeService;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping(Constants.EMPLOYEE_REST_API_BASE_URI_PATH)
@@ -32,5 +36,12 @@ public class EmployeeRestController {
 			.toUri();
 
 		return ResponseEntity.created(createdEmployeeLocation).build();
+	}
+
+	@GetMapping(Constants.RETRIEVE_ACTIVE_MANAGERS_BY_DEPARTMENT_API_URI_PATH)
+	public ResponseEntity<List<ManagerDetailsRetrievalDTO>> getActiveManagersByDepartment(@RequestParam int departmentId) {
+		List<ManagerDetailsRetrievalDTO> activeDepartmentManagersDto = employeeService.getActiveManagersByDepartment(departmentId);
+
+		return ResponseEntity.ok(activeDepartmentManagersDto);
 	}
 }
