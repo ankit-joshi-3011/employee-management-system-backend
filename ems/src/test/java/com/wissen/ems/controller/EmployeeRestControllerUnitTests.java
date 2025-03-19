17d2b60774dad631813e752b969fb345d7208c06
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-19 13:39:36 +0530

    Added a unit test to verify that the getActiveManagersByDepartment() EmployeeRestController method is working as expected

--- ems/src/test/java/com/wissen/ems/controller/EmployeeRestControllerUnitTests.java
+++ ems/src/test/java/com/wissen/ems/controller/EmployeeRestControllerUnitTests.java
@@ -1,10 +1,14 @@
 package com.wissen.ems.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
@@ -18,7 +22,9 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wissen.ems.common.Constants;
+import com.wissen.ems.common.TestUtility;
 import com.wissen.ems.dto.EmployeeDetailsDTO;
+import com.wissen.ems.dto.ManagerDetailsRetrievalDTO;
 import com.wissen.ems.dto.RegularEmployeeDetailsDTO;
 import com.wissen.ems.entity.Department;
 import com.wissen.ems.entity.Employee;
@@ -33,12 +39,15 @@ import com.wissen.ems.service.EmployeeService;
 public class EmployeeRestControllerUnitTests {
 	private MockMvc mockMvc;
 
+	private ObjectMapper objectMapper;
+
 	@MockitoBean
 	private EmployeeService employeeService;
 
 	@Autowired
-	public EmployeeRestControllerUnitTests(MockMvc mockMvc) {
+	public EmployeeRestControllerUnitTests(MockMvc mockMvc, ObjectMapper objectMapper) {
 		this.mockMvc = mockMvc;
+		this.objectMapper = objectMapper;
 	}
 
 	@Test
@@ -90,5 +99,41 @@ public class EmployeeRestControllerUnitTests {
 		String locationHeaderValue = result.getResponse().getHeader(HttpHeaders.LOCATION);
 
 		AssertionsForClassTypes.assertThat(locationHeaderValue).contains(Constants.EMPLOYEE_REST_API_BASE_URI_PATH + "/" + newEmployeeId);
+	}
+
+	@Test
+	public void testGetActiveManagersByDepartment() throws Exception {
+		ManagerDetailsRetrievalDTO headOfEngineeringDto = new ManagerDetailsRetrievalDTO();
+
+		int headOfEngineeringEmployeeId = 2;
+		headOfEngineeringDto.setId(headOfEngineeringEmployeeId);
+
+		String headOfEngineeringName = "DEF";
+		headOfEngineeringDto.setName(headOfEngineeringName);
+
+		ManagerDetailsRetrievalDTO softwareEngineeringManagerDto = new ManagerDetailsRetrievalDTO();
+
+		int softwareEngineeringManagerEmployeeId = 5;
+		softwareEngineeringManagerDto.setId(softwareEngineeringManagerEmployeeId);
+
+		String softwareEngineeringManagerName = "MNO";
+		softwareEngineeringManagerDto.setName(softwareEngineeringManagerName);
+
+		List<ManagerDetailsRetrievalDTO> activeEngineeringManagersDto = List.of(headOfEngineeringDto, softwareEngineeringManagerDto);
+
+		when(employeeService.getActiveManagersByDepartment(anyInt())).thenReturn(activeEngineeringManagersDto);
+
+		int engineeringDepartmentId = 1;
+
+		MvcResult result = mockMvc.perform(get(Constants.EMPLOYEE_REST_API_BASE_URI_PATH + Constants.RETRIEVE_ACTIVE_MANAGERS_BY_DEPARTMENT_API_URI_PATH)
+			.param("departmentId", String.valueOf(engineeringDepartmentId))
+			.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andReturn();
+
+		String jsonResponse = result.getResponse().getContentAsString();
+		List<ManagerDetailsRetrievalDTO> responseActiveEngineeringManagersDto = objectMapper.readValue(jsonResponse, objectMapper.getTypeFactory().constructCollectionType(List.class, ManagerDetailsRetrievalDTO.class));
+
+		TestUtility.assertManagerDetailsRetrievalDtosAreCorrect(responseActiveEngineeringManagersDto);
 	}
 }
